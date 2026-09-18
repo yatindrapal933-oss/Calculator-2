1c48c7f772da366c7016b0cc1c3e7f905e41212b
yatindrapal933-oss <yatindrapal933@gmail.com>
2026-09-18 23:30:51 +0530

    calculator.py
    This only for ethical use

---
+++ python calculator.py
@@ -0,0 +1,10 @@
+number1 = int(input("pehela number likho:"))
+number2 = int(input("dusra number likho:"))
+
+jod = number1 + number2
+guna = number1 * number2
+
+print("______________________________")
+print("dono numbers ka jod (sum) hai :",  jod )
+print("dono numbers ka guna (product) hai :", guna)
+print("______________________________")
